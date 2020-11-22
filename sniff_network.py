931cecc6a8cc0032a69c604e85cb10e0cd5b4c73
Marwane CHAHOUD <marwane.chahoud@orange.com>
2020-11-22 22:00:44 +0100

    function to sniff device in local network

--- sniff_network.py
+++ sniff_network.py
@@ -1,12 +1,21 @@
 from who_is_on_my_wifi import *
+import config
 
+def sniff(mac_addr):
+    WHO = who()
+    #print(WHO)
+    for i in range(0, len(WHO)):
+        for j in WHO[i]:
+            if mac_addr == j:
+                print("Found your device with mac addr = "+ j +" device = " + WHO[i][5])
+                return True
+            else:
+                print("Nothing found, you are out !")
+                return False
 
-WHO = who()
-mac_addr = "my mac addr"
-#print(WHO)
-for i in range(0, len(WHO)):
-    for j in WHO[i]:
-        if mac_addr == j:
-            print("Found your device with mac addr = "+ j +" device = " + WHO[i][5])
-            break
-    #print(WHO[i])
+# # sniff(config.MAC_ADR)
+#
+# WHO = who()
+# #print(WHO)
+# for i in range(0, len(WHO)):
+#     print(WHO[i])
