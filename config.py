ae3127424ab613e0cca0001a16d3b5b18d60be92
Marwane Chahoud <marwane.chahoud@gmail.com>
2020-11-22 22:46:57 +0100

    ajouter config.py

---
+++ config.py
@@ -0,0 +1,19 @@
+# configuration du gmail
+EMAIL = "camille.dupont@gmail.com"
+SUJET_MAIL = "Attestation dérogatoire"
+BODY_MAIL = "Attestation dérogatoire"
+PWD = "VotreMotDePasseGMAIL"
+
+# informations perso
+
+NOM = "Dupont"
+PRENOM = "Camille"
+DDN = "01/01/1970" # date de naissance
+LDN = "Paris" # lieu de naissance
+VILLE = "Paris"
+ADDR = "999 avenue de France"
+CP = "75001"
+
+# information tél
+
+MAC_ADR = "00:BZ:XX:XX:23:H6" # adresse mac de votre téléphone
